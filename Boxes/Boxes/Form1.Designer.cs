0991ae1055217f97ba8c71389ee027df333b10aa
Balytsky Igor <ibalyts@live.com>
2020-05-30 17:08:10 +0300

    Bal 30-05

--- Boxes/Boxes/Form1.Designer.cs
+++ Boxes/Boxes/Form1.Designer.cs
@@ -33,33 +33,30 @@
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.pnlTable = new System.Windows.Forms.Panel();
             this.pnlDeck = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // pnlPlayer1
             // 
-            this.pnlPlayer1.Location = new System.Drawing.Point(7, 22);
-            this.pnlPlayer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlPlayer1.Location = new System.Drawing.Point(5, 18);
             this.pnlPlayer1.Name = "pnlPlayer1";
-            this.pnlPlayer1.Size = new System.Drawing.Size(939, 316);
+            this.pnlPlayer1.Size = new System.Drawing.Size(704, 257);
             this.pnlPlayer1.TabIndex = 0;
             // 
             // pnlPlayer2
             // 
-            this.pnlPlayer2.Location = new System.Drawing.Point(9, 346);
-            this.pnlPlayer2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlPlayer2.Location = new System.Drawing.Point(7, 281);
             this.pnlPlayer2.Name = "pnlPlayer2";
-            this.pnlPlayer2.Size = new System.Drawing.Size(935, 337);
+            this.pnlPlayer2.Size = new System.Drawing.Size(701, 274);
             this.pnlPlayer2.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(975, 22);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(731, 18);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
@@ -68,67 +65,63 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(975, 91);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Location = new System.Drawing.Point(731, 74);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(96, 24);
+            this.comboBox1.Size = new System.Drawing.Size(73, 21);
             this.comboBox1.TabIndex = 3;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(975, 137);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox2.Location = new System.Drawing.Point(731, 111);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(96, 24);
+            this.comboBox2.Size = new System.Drawing.Size(73, 21);
             this.comboBox2.TabIndex = 4;
             this.comboBox2.Visible = false;
             // 
-            // comboBox3
-            // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(975, 177);
-            this.comboBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(96, 24);
-            this.comboBox3.TabIndex = 5;
-            this.comboBox3.Visible = false;
-            // 
             // pnlTable
             // 
-            this.pnlTable.Location = new System.Drawing.Point(975, 241);
-            this.pnlTable.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlTable.Location = new System.Drawing.Point(731, 196);
             this.pnlTable.Name = "pnlTable";
-            this.pnlTable.Size = new System.Drawing.Size(199, 134);
+            this.pnlTable.Size = new System.Drawing.Size(149, 109);
             this.pnlTable.TabIndex = 6;
             // 
             // pnlDeck
             // 
-            this.pnlDeck.Location = new System.Drawing.Point(975, 409);
-            this.pnlDeck.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlDeck.Location = new System.Drawing.Point(731, 332);
             this.pnlDeck.Name = "pnlDeck";
-            this.pnlDeck.Size = new System.Drawing.Size(242, 134);
+            this.pnlDeck.Size = new System.Drawing.Size(182, 109);
             this.pnlDeck.TabIndex = 7;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(731, 148);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1232, 692);
+            this.ClientSize = new System.Drawing.Size(924, 562);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.pnlDeck);
             this.Controls.Add(this.pnlTable);
-            this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pnlPlayer2);
             this.Controls.Add(this.pnlPlayer1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -139,9 +132,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.Panel pnlTable;
         private System.Windows.Forms.Panel pnlDeck;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
