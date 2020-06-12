b9288c46c724c014e55b35728cd2a3e921ca6df5
Kristina-Silvanovych <kristina.silvanovych@gmail.com>
2020-06-12 13:56:47 +0300

    Changes12

--- Boxes/Boxes/Form1.Designer.cs
+++ Boxes/Boxes/Form1.Designer.cs
@@ -33,7 +33,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.cmbFigure = new System.Windows.Forms.ComboBox();
             this.cmbAmount = new System.Windows.Forms.ComboBox();
-            this.Go = new System.Windows.Forms.Button();
+            this.btncheck = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox3 = new System.Windows.Forms.CheckBox();
@@ -43,32 +43,33 @@
             this.pnlPlayer3 = new System.Windows.Forms.Panel();
             this.pnlPlayer4 = new System.Windows.Forms.Panel();
             this.pnlDeck = new System.Windows.Forms.Panel();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
             this.gbxSuits.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlPlayer1
             // 
-            this.pnlPlayer1.Location = new System.Drawing.Point(25, 39);
-            this.pnlPlayer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlPlayer1.Location = new System.Drawing.Point(19, 78);
             this.pnlPlayer1.Name = "pnlPlayer1";
-            this.pnlPlayer1.Size = new System.Drawing.Size(309, 206);
+            this.pnlPlayer1.Size = new System.Drawing.Size(281, 167);
             this.pnlPlayer1.TabIndex = 0;
             // 
             // pnlPlayer2
             // 
             this.pnlPlayer2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.pnlPlayer2.Location = new System.Drawing.Point(439, 39);
-            this.pnlPlayer2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlPlayer2.Location = new System.Drawing.Point(331, 78);
             this.pnlPlayer2.Name = "pnlPlayer2";
-            this.pnlPlayer2.Size = new System.Drawing.Size(307, 206);
+            this.pnlPlayer2.Size = new System.Drawing.Size(281, 167);
             this.pnlPlayer2.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(975, 22);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(731, 78);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(80, 32);
             this.button1.TabIndex = 2;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
@@ -77,10 +78,9 @@
             // cmbFigure
             // 
             this.cmbFigure.FormattingEnabled = true;
-            this.cmbFigure.Location = new System.Drawing.Point(975, 76);
-            this.cmbFigure.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbFigure.Location = new System.Drawing.Point(731, 132);
             this.cmbFigure.Name = "cmbFigure";
-            this.cmbFigure.Size = new System.Drawing.Size(96, 24);
+            this.cmbFigure.Size = new System.Drawing.Size(80, 21);
             this.cmbFigure.TabIndex = 3;
             this.cmbFigure.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             this.cmbFigure.TextChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
@@ -93,34 +93,32 @@
             "2",
             "3",
             "4"});
-            this.cmbAmount.Location = new System.Drawing.Point(975, 124);
-            this.cmbAmount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbAmount.Location = new System.Drawing.Point(731, 178);
             this.cmbAmount.Name = "cmbAmount";
-            this.cmbAmount.Size = new System.Drawing.Size(96, 24);
+            this.cmbAmount.Size = new System.Drawing.Size(80, 21);
             this.cmbAmount.TabIndex = 4;
             this.cmbAmount.Visible = false;
             this.cmbAmount.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             this.cmbAmount.TextChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
-            // Go
+            // btncheck
             // 
-            this.Go.Enabled = false;
-            this.Go.Location = new System.Drawing.Point(975, 356);
-            this.Go.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Go.Name = "Go";
-            this.Go.Size = new System.Drawing.Size(107, 39);
-            this.Go.TabIndex = 8;
-            this.Go.Text = "button2";
-            this.Go.UseVisualStyleBackColor = true;
-            this.Go.Click += new System.EventHandler(this.button2_Click);
+            this.btncheck.Enabled = false;
+            this.btncheck.Location = new System.Drawing.Point(731, 388);
+            this.btncheck.Margin = new System.Windows.Forms.Padding(2);
+            this.btncheck.Name = "btncheck";
+            this.btncheck.Size = new System.Drawing.Size(80, 32);
+            this.btncheck.TabIndex = 8;
+            this.btncheck.Text = "Check";
+            this.btncheck.UseVisualStyleBackColor = true;
+            this.btncheck.Click += new System.EventHandler(this.button2_Click);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(21, 32);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox1.Location = new System.Drawing.Point(16, 26);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(86, 21);
+            this.checkBox1.Size = new System.Drawing.Size(68, 17);
             this.checkBox1.TabIndex = 9;
             this.checkBox1.Text = "Diamond";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -128,10 +126,9 @@
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(21, 60);
-            this.checkBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox2.Location = new System.Drawing.Point(16, 49);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(58, 21);
+            this.checkBox2.Size = new System.Drawing.Size(47, 17);
             this.checkBox2.TabIndex = 10;
             this.checkBox2.Text = "Club";
             this.checkBox2.UseVisualStyleBackColor = true;
@@ -139,10 +136,9 @@
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(21, 117);
-            this.checkBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox3.Location = new System.Drawing.Point(16, 95);
             this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(71, 21);
+            this.checkBox3.Size = new System.Drawing.Size(57, 17);
             this.checkBox3.TabIndex = 11;
             this.checkBox3.Text = "Spade";
             this.checkBox3.UseVisualStyleBackColor = true;
@@ -150,10 +146,9 @@
             // checkBox4
             // 
             this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(21, 89);
-            this.checkBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox4.Location = new System.Drawing.Point(16, 72);
             this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(65, 21);
+            this.checkBox4.Size = new System.Drawing.Size(52, 17);
             this.checkBox4.TabIndex = 12;
             this.checkBox4.Text = "Heart";
             this.checkBox4.UseVisualStyleBackColor = true;
@@ -164,11 +159,11 @@
             this.gbxSuits.Controls.Add(this.checkBox4);
             this.gbxSuits.Controls.Add(this.checkBox2);
             this.gbxSuits.Controls.Add(this.checkBox3);
-            this.gbxSuits.Location = new System.Drawing.Point(975, 164);
-            this.gbxSuits.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gbxSuits.Location = new System.Drawing.Point(731, 225);
+            this.gbxSuits.Margin = new System.Windows.Forms.Padding(2);
             this.gbxSuits.Name = "gbxSuits";
-            this.gbxSuits.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.gbxSuits.Size = new System.Drawing.Size(200, 164);
+            this.gbxSuits.Padding = new System.Windows.Forms.Padding(2);
+            this.gbxSuits.Size = new System.Drawing.Size(150, 133);
             this.gbxSuits.TabIndex = 13;
             this.gbxSuits.TabStop = false;
             this.gbxSuits.Text = "Suits";
@@ -177,52 +172,86 @@
             // lblMessage
             // 
             this.lblMessage.AutoSize = true;
-            this.lblMessage.Location = new System.Drawing.Point(985, 546);
+            this.lblMessage.Location = new System.Drawing.Point(739, 444);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(0, 17);
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
             this.lblMessage.TabIndex = 14;
             // 
             // pnlPlayer3
             // 
-            this.pnlPlayer3.Location = new System.Drawing.Point(35, 322);
-            this.pnlPlayer3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlPlayer3.Location = new System.Drawing.Point(19, 312);
             this.pnlPlayer3.Name = "pnlPlayer3";
-            this.pnlPlayer3.Size = new System.Drawing.Size(365, 206);
+            this.pnlPlayer3.Size = new System.Drawing.Size(281, 167);
             this.pnlPlayer3.TabIndex = 15;
             // 
             // pnlPlayer4
             // 
-            this.pnlPlayer4.Location = new System.Drawing.Point(439, 322);
-            this.pnlPlayer4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlPlayer4.Location = new System.Drawing.Point(331, 312);
             this.pnlPlayer4.Name = "pnlPlayer4";
-            this.pnlPlayer4.Size = new System.Drawing.Size(365, 206);
+            this.pnlPlayer4.Size = new System.Drawing.Size(281, 167);
             this.pnlPlayer4.TabIndex = 16;
             // 
             // pnlDeck
             // 
-            this.pnlDeck.Location = new System.Drawing.Point(973, 466);
-            this.pnlDeck.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlDeck.Location = new System.Drawing.Point(634, 238);
             this.pnlDeck.Name = "pnlDeck";
-            this.pnlDeck.Size = new System.Drawing.Size(113, 132);
+            this.pnlDeck.Size = new System.Drawing.Size(10, 133);
             this.pnlDeck.TabIndex = 1;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(25, 33);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 13);
+            this.lbl1.TabIndex = 17;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(337, 33);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(0, 13);
+            this.lbl2.TabIndex = 18;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(25, 278);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(0, 13);
+            this.lbl3.TabIndex = 19;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(337, 278);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(0, 13);
+            this.lbl4.TabIndex = 20;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1232, 692);
-            this.Controls.Add(this.pnlDeck);
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(924, 562);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
             this.Controls.Add(this.pnlPlayer4);
+            this.Controls.Add(this.pnlDeck);
             this.Controls.Add(this.pnlPlayer3);
             this.Controls.Add(this.lblMessage);
             this.Controls.Add(this.gbxSuits);
-            this.Controls.Add(this.Go);
+            this.Controls.Add(this.btncheck);
             this.Controls.Add(this.cmbAmount);
             this.Controls.Add(this.cmbFigure);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pnlPlayer2);
             this.Controls.Add(this.pnlPlayer1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -240,7 +269,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox cmbFigure;
         private System.Windows.Forms.ComboBox cmbAmount;
-        private System.Windows.Forms.Button Go;
+        private System.Windows.Forms.Button btncheck;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox3;
@@ -250,6 +279,10 @@
         private System.Windows.Forms.Panel pnlPlayer3;
         private System.Windows.Forms.Panel pnlPlayer4;
         private System.Windows.Forms.Panel pnlDeck;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
     }
 }
 
